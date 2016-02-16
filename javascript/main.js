d08ee4bd11417f364ee7f848f751155c004a3185
Matthew <mw@matthewis.me>
2016-02-16 14:57:47 -0500

    add functionality for enter key

--- javascript/main.js
+++ javascript/main.js
@@ -8,15 +8,30 @@
 // When the user clicks on a list item, it is removed
 // Extra Credit: - When a list item is clicked, cross it out, then remove it after 1 second.
 // get value of the text input
-var input = document.getElementById('input');
+var inputBox = document.getElementById('input');
 var toDoList = document.querySelector('ul');
 var addNewItemBtn = document.querySelector('#btn');
+var newItem = document.createElement("li")
 
-addNewItemBtn.addEventListener('click',function(event) {
-  var newItem = document.createElement("li")
-  newItem.innerHTML = '<a href="#">' + input.value +'</a>'
-  toDoList.appendChild(newItem);
-  input.value = "";
+function addNewItem() {
+	if (inputBox.value == '' || inputBox.value == inputBox.defaultValue) {
+		alert('Please add a new item.');	
+	} else {
+	  newItem.innerHTML = '<a href="#">' + inputBox.value +'</a>'
+	  toDoList.appendChild(newItem);
+	  inputBox.value = "";	
+	}
+}
+
+addNewItemBtn.addEventListener('click', addNewItem);
+// add functionality for 'enter' key
+inputBox.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      addNewItem();
+    } else {
+    	// do nothing
+    }
 });
 
 toDoList.addEventListener('click', function(event){
